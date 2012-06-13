2d1e09e06513d01278292c2f95b662d983e1e50e
dainius <dainius@ubuntu.(none)>
2012-06-13 05:04:54 -0700

    pakrauna statini turini

--- inovatyviospratybos/hello/hello.go
+++ inovatyviospratybos/hello/hello.go
@@ -15,6 +15,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 "<html>" +
   "<body bgcolor='silver'>" +
     "<h1>Prasideda inovatyviu pratybu programavimo darbai</h1>" +
+    "<a href='./images/index.html'>Paziurekite ponai koki puiku html turiu</a>" +
   "</body>" +
 "</html>"
 
